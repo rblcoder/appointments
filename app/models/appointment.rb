47a150669e4406a706cc2ce1775ce86410a3a5c6
rblcoder <rupacoach@gmail.com>
2024-04-18 14:09:24 +0000

    Added models

---
+++ app/models/appointment.rb
@@ -0,0 +1,8 @@
+class Appointment < ApplicationRecord
+  belongs_to :doctor
+  belongs_to :patient
+
+  validates :start_time, :end_time, presence: true
+  
+
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # Allow requests from the Gitpod subdomain
   config.hosts.clear
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
